21c7211d81f76b346aeee9f421e74eedefbc530d
Ariful Islam <arifulabir.work@gmail.com>
2026-09-04 21:14:31 +1000

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+python_creation_date = "February, 1991"
+java_Creation_Date = "May, 1995"
+javascript_Creation_Date = "December, 1995"
+
+print(python_creation_date)
+print(java_Creation_Date)
+print(javascript_Creation_Date)
